286df584e65b6e175983e21a919a71ae3cedca61
sa02045 <50866506+sa02045@users.noreply.github.com>
2020-08-09 21:23:24 +0900

    sa02045 백준 9521 풀이실패
    sa02045 백준 9521 풀이실패

---
+++ Algorithm/백준 9521/sa02045/sa02045Solution.py
@@ -0,0 +1,8 @@
+import sys
+max = 1000000007
+if __name__ == '__main__':
+    N,K = map(int,sys.stdin.readline().split())
+    for i in range(N):
+        fi = list(map(int,sys.stdin.readline().split()))
+
+    # ??
